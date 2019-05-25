6a673679b7a6e081ee766dc783ec36e979e78208
dotjava <wisenow@163.com>
2019-05-25 08:57:32 +0800

    调整包遗留问题

--- http.go
+++ http.go
@@ -9,7 +9,7 @@ import (
 var def *gotserv.Router
 
 func init() {
-	def = NewRouter()
+	def = gotserv.NewRouter()
 }
 
 func checkInit() {
@@ -19,37 +19,37 @@ func checkInit() {
 }
 
 //DefaultRouter 获取默认的 Router
-func DefaultRouter() *Router {
+func DefaultRouter() *gotserv.Router {
 	checkInit()
 	return def
 }
 
 //GET 在默认的 defaultConetx 上注册 GET 请求
-func GET(path string, handler Handler) {
+func GET(path string, handler gotserv.Handler) {
 	checkInit()
 	def.GET(path, handler)
 }
 
 //POST 在默认的 defaultConetx 上注册 POST 请求
-func POST(path string, handler Handler) {
+func POST(path string, handler gotserv.Handler) {
 	checkInit()
 	def.POST(path, handler)
 }
 
 //HEAD 在默认的 defaultConetx 上注册 HEAD 请求
-func HEAD(path string, handler Handler) {
+func HEAD(path string, handler gotserv.Handler) {
 	checkInit()
 	def.HEAD(path, handler)
 }
 
 //PUT 在默认的 defaultConetx 上注册 PUT 请求
-func PUT(path string, handler Handler) {
+func PUT(path string, handler gotserv.Handler) {
 	checkInit()
 	def.PUT(path, handler)
 }
 
 //DELETE 在默认的 defaultConetx 上注册 DELETE 请求
-func DELETE(path string, handler Handler) {
+func DELETE(path string, handler gotserv.Handler) {
 	checkInit()
 	def.DELETE(path, handler)
 }
