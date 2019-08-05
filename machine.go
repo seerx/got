0ac31e045a849e0028424f3be335f26110af0542
dotjava <wisenow@163.com>
2019-08-05 09:50:20 +0800

    增加计算绝对路径函数

--- machine.go
+++ machine.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -48,6 +49,12 @@ func DirecotoryExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+// TrimAbsolutePath 计算真实路径，去掉 .. 之类的目录层次
+func TrimAbsolutePath(path string) string {
+	paths := strings.Split(path, "/")
+	return filepath.Join(append([]string{d.RootPath}, paths...)...)
 }
 
 //MakeDirecotories 连续创建路径
